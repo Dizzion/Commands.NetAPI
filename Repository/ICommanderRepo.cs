c9b6c251ca80d67367ea89d3865a3858ec98e133
AShaver43 <gofrodude_197@hotmail.com>
2020-09-06 23:24:23 -0700

    DTO and Read functions done

---
+++ Repository/ICommanderRepo.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Repository
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+    }
+}
---
+++ Repository/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Repository
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil Water", Platform="Kettle & Pan"},
+                new Command{Id=1, HowTo="Boil a Steak", Line="Boil Water", Platform="Kettle & Pan"},
+                new Command{Id=2, HowTo="Boil a Pot", Line="Boil Water", Platform="Kettle & Pan"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil Water", Platform="Kettle & Pan"};
+        }
+    }
+}
