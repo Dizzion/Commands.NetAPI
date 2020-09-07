d2c7dfc1c89c0021a996847b796edd5e1384e0f2
AShaver43 <gofrodude_197@hotmail.com>
2020-09-07 00:14:54 -0700

    Update REPO

--- Repository/ICommanderRepo.cs
+++ Repository/ICommanderRepo.cs
@@ -10,5 +10,6 @@ namespace Commander.Repository
         IEnumerable<Command> GetAllCommands();
         Command GetCommandById(int id);
         void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
     }
 }
